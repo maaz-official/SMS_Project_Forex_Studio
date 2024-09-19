7a675c3ddfd960973d7d4e6957ca488141a25e29
Muhammd Maaz <161757387+maaz-official@users.noreply.github.com>
2024-09-19 14:39:40 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors({ origin: 'https://sms-project-forex-studio.vercel.app' }));
 
 mongoose
     .connect(process.env.MONGO_URL, {
