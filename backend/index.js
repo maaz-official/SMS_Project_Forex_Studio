678ce92abb28603d55a7ed0eede5e0c0059309cc
Muhammd Maaz <161757387+maaz-official@users.noreply.github.com>
2024-09-20 11:20:51 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,9 +15,12 @@ dotenv.config();
 
 app.use(express.json({ limit: '10mb' }))
 
-app.use(cors({
-    origin: "https://sms-project-forex-studio.vercel.app"
-}));
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 
 mongoose
